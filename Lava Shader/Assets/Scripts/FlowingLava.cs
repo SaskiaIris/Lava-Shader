c5b801c3ac9b56123ed0d6cbc70ff2c99318d15a
Iris <iris.oostra@hva.nl>
2021-03-08 15:44:21 +0100

    Het begin is er

---
+++ Lava Shader/Assets/Scripts/FlowingLava.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowingLava : MonoBehaviour
+{
+    public float scrollSpeed = 0.1f;
+    Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveThis = Time.time * scrollSpeed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(0, moveThis));
+    }
+}
