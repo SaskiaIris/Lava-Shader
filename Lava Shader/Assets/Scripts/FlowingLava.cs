776f06f8e2e624110c2aef9de578e88755a3adf4
Iris <iris.oostra@hva.nl>
2021-03-11 15:38:19 +0100

    Changed the character

--- Lava Shader/Assets/Scripts/FlowingLava.cs
+++ Lava Shader/Assets/Scripts/FlowingLava.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class FlowingLava : MonoBehaviour
 {
     public float scrollSpeed = 0.1f;
-    Renderer renderer;
+    Renderer rendererComp;
 
     // Start is called before the first frame update
     void Start()
     {
-        renderer = GetComponent<Renderer>();
+        rendererComp = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float moveThis = Time.time * scrollSpeed;
-        renderer.material.SetTextureOffset("_MainTex", new Vector2(0, moveThis));
+        rendererComp.material.SetTextureOffset("_MainTex", new Vector2(0, moveThis));
     }
 }
--- Lava Shader/Assets/Scripts/PlayerMotor.cs
+++ Lava Shader/Assets/Scripts/PlayerMotor.cs
@@ -5,23 +5,27 @@ using System.Collections;
 //[RequireComponent(typeof(CharacterController))]
 
 public class PlayerMotor : MonoBehaviour {
-    public float forwardSpeed = 0.5f;
-    public float backwardSpeed = 0.2f;
-    public float rotateSpeed = 0.2f;
-    //public float jumpSpeed = 8.0f;
+    public float forwardSpeed = 2f;
+    public float backwardSpeed = 1f;
+    public float rotateSpeed = 1f;
+    public float jumpSpeed = 6f;
 
 
     private Vector3 velocity;
     private float h;
     private float v;
 
+    private bool isJumping;
+
 
     Animator animator;
-    //CharacterController controller;
+    Rigidbody rigidBody;
 
     private void Start() {
         animator = GetComponent<Animator>();
-        //controller = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        isJumping = false;
     }
 
 	void Update() {
@@ -46,14 +50,26 @@ public class PlayerMotor : MonoBehaviour {
             transform.Rotate(0, h * rotateSpeed, 0);
         }
         //Jumping
-        /*if(Input.GetButtonDown("Jump")) {
-                velocity.y = jumpSpeed;
-                animator.SetBool("Jump", true);
-            }
-            if(Input.GetButtonUp("Jump")) {
-                animator.SetBool("Jump", false);
-            }*/
+        if(Input.GetButton("Jump") && isJumping == false) {
+            //velocity.y = jumpSpeed;
+            rigidBody.AddForce(new Vector3(0,jumpSpeed,0), ForceMode.Impulse);
+            animator.SetBool("Jump", true);
+            isJumping = true;
+        }
 
         transform.localPosition += velocity * Time.fixedDeltaTime;
     }
+
+	private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.transform.tag == "Floor") {
+            isJumping = false;
+            animator.SetBool("Jump", false);
+        } else {
+        
+        }
+	}
+
+	/*private void OnCollisionEnter(Collision collision) {
+        isJumping = false;
+	}*/
 }
