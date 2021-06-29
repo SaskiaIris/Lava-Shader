edb595f2f58480029c1c1ee0aee4c4c9157282ad
Iris <iris.oostra@hva.nl>
2021-06-29 16:49:50 +0200

    Trying to randomize particles

---
+++ Lava Shader/Assets/RandomParticles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomParticles : MonoBehaviour {
+    [SerializeField]
+    private int particleRate = 1;
+
+    [SerializeField]
+    private GameObject particlePrefab;
+
+    public Transform[] SpawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform selectedSpawnPoint = SpawnPoints[(int)Random.Range(0, SpawnPoints.Count - 1)];
+        Instantiate(particlePrefab, selectedSpawnPoint.position, selectedSpawnPoint.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
