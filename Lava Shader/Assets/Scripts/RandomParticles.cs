1212c6dcbc27129a5120466f092122bbf48dd7d2
Iris <iris.oostra@hva.nl>
2021-07-01 12:42:32 +0200

    Added a lot of customization, randomizations and improvements to the particle spawn script

--- Lava Shader/Assets/Scripts/RandomParticles.cs
+++ Lava Shader/Assets/Scripts/RandomParticles.cs
@@ -4,40 +4,101 @@ using UnityEngine;
 
 public class RandomParticles : MonoBehaviour {
     [SerializeField]
-    private int particleRate = 1;
+    private int minParticlesToSpawn = 1;
+
+    [SerializeField]
+    private int maxParticlesToSpawn = 5;
+
+    [SerializeField]
+    private string spawnTag = "Spawnable";
+
+    [SerializeField]
+    private int maxParticlesOnScreen = 10;
+
+    [SerializeField]
+    private float spawnIntervalMin = 2.0f;
+
+    [SerializeField]
+    private float spawnIntervalMax = 8.0f;
 
     [SerializeField]
     private GameObject particlePrefab;
 
     [SerializeField]
-    private List<GameObject> spawnPool;
-
-    [SerializeField]
     private GameObject lavaField;
 
+    private GameObject[] currentParticleSystems;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        SpawnParticles();
+    void Start() {
+        StartCoroutine("SpawnParticles");
+
+        if(maxParticlesOnScreen > lavaField.transform.localScale.x) {
+            maxParticlesOnScreen = (int) lavaField.transform.localScale.x;
+        }
+
+        Debug.Log("Max particles: " + maxParticlesOnScreen);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 
-    private void SpawnParticles()
-	{
-        DestroyTimedOutParticles();
-        int randomItem = 0;
+    IEnumerator SpawnParticles() {
+        int particleAmountToSpawn;
         GameObject toSpawn;
         MeshCollider lavaMeshCollider = lavaField.GetComponent<MeshCollider>();
 
+        float screenX, screenY, screenZ;
+        Vector3 smokePosition;
+
+        float spawnInterval;
+
+        for(; ; ) {
+            DestroyTimedOutParticles();
+
+            if(Random.Range(0, 5) == 0) {
+                particleAmountToSpawn = 0;
+            } else {
+                particleAmountToSpawn = Random.Range(minParticlesToSpawn, maxParticlesToSpawn);
+            }
+
+            for(int i = 0; i < particleAmountToSpawn; i++) {
+                toSpawn = particlePrefab;
+                screenX = Random.Range(lavaMeshCollider.bounds.min.x, lavaMeshCollider.bounds.max.x);
+                screenY = Random.Range(lavaMeshCollider.bounds.min.y, lavaMeshCollider.bounds.max.y);
+                screenZ = Random.Range(lavaMeshCollider.bounds.min.z, lavaMeshCollider.bounds.max.z);
+                smokePosition = new Vector3(screenX, screenY, screenZ);
+
+                Instantiate(toSpawn, smokePosition, lavaField.transform.rotation);
+
+                yield return new WaitForSeconds(Random.Range(0.0f, 0.5f));
+            }
+
+            spawnInterval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private void DestroyTimedOutParticles()	{
+        currentParticleSystems = GameObject.FindGameObjectsWithTag(spawnTag);
+        int randomizeMaxParticles = Random.Range(maxParticlesOnScreen - 3, maxParticlesOnScreen);
+        foreach(GameObject o in currentParticleSystems) {
+            if(o.GetComponent<ParticleSystem>().isPlaying && currentParticleSystems.Length <= randomizeMaxParticles) {
+                //Do nothing
+            } else if(o.GetComponent<ParticleSystem>().isPlaying && currentParticleSystems.Length <= randomizeMaxParticles) {
+                FadeOut(o);
+            } else {
+                Destroy(o);
+            }
+		}
 	}
 
-    private void DestroyTimedOutParticles()
-	{
-
+	private void FadeOut(GameObject particleToFadeOut) {
+        
+        Destroy(particleToFadeOut);
 	}
+
 }
